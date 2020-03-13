04f7ffde7ca39d00afdf6e21b77cef8aafa584bf
Claire Badger <claire.badger@afit.edu>
2020-03-12 23:33:54 -0400

    More setup along guidelines of RFC

--- Node.py
+++ Node.py
@@ -7,6 +7,8 @@ class Node():
         self.port_num = port
         self.messages = queue #not sure if this is right, feel free to replace. we just want a message queue
         #make list of other nodes on the network
+        self.activeNodeList = []
+        self.activeClientList = []
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             self.socket = s
             s.bind(('', port))
@@ -14,7 +16,17 @@ class Node():
 
     def heartbeat(self):
         #send out messages to the other nodes, make sure they are still alive
+        for node in self.activeNodeList:
+            self.socket.send("heartbeat".encode())
         return
+
+    def returnIP(self):
+        ip = 0
+        return ip
+
+    def connectNode(self, ip):
+        self.socket.connect((ip, self.port_num))
+        self.activeNodeList.append(ip)
 
     def solve(self, number):
         #number is the number to be prime factorized
@@ -24,6 +36,7 @@ class Node():
 
     def acceptClient(self):
         self.socket.accept()
+        self.activeClientList.append() #when you get internet, see if accept() returns something to append to the list
 
     def getWork(self):
         data = self.socket.recv(1024)
@@ -42,14 +55,18 @@ class Node():
 
 
 def main(self):
-    server_list = {'0.0.0.0', '127.0.0.1'}
+    #this code is run on each server
+    node_list = {'0.0.0.0', '127.0.0.1', '0.0.0.0'}
 
     port = 9999
 
-    node_list = []
-
     node = Node(port)
 
+    for currNode in node_list: #connect every node to every other node
+        if currNode != node.returnIP():#check to make sure ip isnt the node's ip
+            node.connectNode(currNode)
+
+    #listen for new connections from clients, send messages to other nodes as needed
     while True:
         node.acceptClient()
 
