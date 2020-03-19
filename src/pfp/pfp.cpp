086b9516378eb027651a39e24a1924f7992193e3
Claire Badger <claire.badger@afit.edu>
2020-03-18 22:13:07 -0700

    fixing errors

--- src/pfp/pfp.cpp
+++ src/pfp/pfp.cpp
@@ -1,4 +1,4 @@
-#include "pfp.h"
+#include <pfp/pfp.h>
 
 
 namespace pfp
@@ -81,7 +81,7 @@ void WorkOrder::decode(std::istream& in)
 			break;
 		default:
 			std::cerr << "Broken message, can't do anything" << std::endl;
-			algorithm_ = NONE;
+			algorithm_ = static_cast<ALG>(NULL);
 	}
 }
 
